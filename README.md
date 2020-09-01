eb1be15db163e27100e87840bf3f72e4a3d49806
Thomas Anderson <linuxgaming@protonmail.com>
2020-09-01 21:19:18 +0300

    - initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # unturbo
+
+This is a fork of [amp2html](https://github.com/da2x/amp2html) from [Daniel Aleksandersen](https://github.com/da2x)
---
+++ scripts/redirector.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2017–2020 Daniel Aleksandersen <https://www.daniel.priv.no/>
+ * Copyright © 2020 omicron-b <https://github.com/omicron-b/>
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
+
+// Yandex Turbo redirect
+// Convert https://yandex.ru/turbo/s/example.com/document
+//      to https://example.com/document
+
+var browserapp = (typeof browser !== 'undefined') ? browser : chrome;
+
+function turbo_redirector(requestDetails)
+{
+  var turbo = new URL(requestDetails.url);
+  var redirection = turbo.pathname.replace(/^\/turbo\/s\//, 'https://');
+  return {
+    redirectUrl: redirection
+  };
+}
+
+browserapp.webRequest.onBeforeRequest.addListener(
+  turbo_redirector,
+  {
+    urls: [
+      'https://yandex.ru/turbo/*'
+    ]
+  },
+  ["blocking"]
+);
